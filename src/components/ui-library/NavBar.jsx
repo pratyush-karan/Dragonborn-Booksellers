dac0a872aac2c1d2f7ca009a748828bb97d8a42a
Pratyush Karan <pratyushkaran007@gmail.com>
2024-05-27 15:48:41 +0530

    wish list page hidden

--- src/components/ui-library/NavBar.jsx
+++ src/components/ui-library/NavBar.jsx
@@ -24,7 +24,7 @@ import { useSelector } from "react-redux";
 import { usePathname } from "next/navigation";
 
 const pages = ["Home", "Books", "Profile"];
-const settings = ["Your Orders", "Wish List", "Logout"];
+const settings = ["Your Orders", "Logout"];
 
 export default function NavBar({ children }) {
   const router = useRouter();
